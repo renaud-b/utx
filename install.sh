4dd13d0330bd43c955dd9c42997f2013ea274d63
renaud.bataille <renaud.bataille@adevinta.com>
2026-04-24 00:59:41 +0200

    chore: publish v1.1.0 release bundle

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ set -e
 echo "Downloading utx..."
 
 # Qme3mqytCCXk7Z6zNfTiQh12gnH4kAUiz9Kq1wAzth7WEa => utx-linux-amd64-v1.0.0
-curl -L https://metapixia.com/ipfs/Qme3mqytCCXk7Z6zNfTiQh12gnH4kAUiz9Kq1wAzth7WEa -o utx
+# Qmdgb63HtJ2HuXg1id66NGYrVg4NFsuwDxqBnwBiL82SqC => utx-linux-amd64-v1.1.0
+curl -L https://metapixia.com/ipfs/Qmdgb63HtJ2HuXg1id66NGYrVg4NFsuwDxqBnwBiL82SqC -o utx
 
 chmod +x utx
 sudo mv utx /usr/local/bin/
